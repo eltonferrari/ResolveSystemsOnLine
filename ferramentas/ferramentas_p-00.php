46226b702143f2d2470ad29cef952b3a9b6c6e3c
elton ferrari <eltonferrari@gmail.com>
2023-04-18 12:11:46 -0300

    1.0

---
+++ ferramentas/ferramentas_p-00.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="pt-br">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- COMPATIBILIDADE COM HTML5 -->
+		<!--[if lt IE 9]>
+			<script src="js/html5shiv.js"></script>
+		<![endif]-->
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="assets/Bootstrap4/css/bootstrap.min.css">
+
+		<!-- NORMALIZE CSS -->
+		<link   rel="stylesheet" type="text/css" href="assets/css/normalize.css">
+
+		<!-- CSS CUSTOMIZADO -->
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+		<title>RS - Resolve Systems</title>
+	</head>
+	<body class="bg-light">
+		<?php include 'template/menu.php';?>
+		<div class="container conteudo">
+			<div class="d-flex justify-content-center">
+				<h2 class="text-success">Portfólio</h2>
+			</div>
+			<hr class="divisor">
+			<div>
+			<div class="container conteudo">
+			<div>
+				<div class="d-flex justify-content-center">
+					<h2 class="text-success">Portfólio</h2>
+				</div>
+				<hr class="divisor">
+			</div>
+			<div>
+				<div class="row">
+					<div class="col-md-6 m-auto">
+						<h5 class="text-center text-primary">Interação Humano-Computador (IHC)</h5>
+					</div>
+				</div>
+				<p class="recuo-primeira-linha">A Interação Humano-Computador estuda a interação entre pessoas e computadores, 
+					assim como as teorias e técnicas de projeto utilizadas para tornar um sistema 
+					interativo. A IHC é baseada tanto no conhecimento da máquina quanto no lado humano.</p>
+				<p class="recuo-primeira-linha">As interações entre usuários e computadores acontecem na interface, portanto, o 
+					design de uma interface tem impacto direto na eficiência dessas interações.</p>
+				<p class="recuo-primeira-linha">O objetivo é minimizar o custo de interação — a quantidade de esforço físico e mental 
+					que um usuário deve exercer ao usar a tecnologia — e tornar as interações mais humanas.
+				</p>
+				<hr class="divisor">
+				<div class="row">
+					<div class="col-md-6 m-auto text-center">
+						<a href="portfolio/portfolio_p-01.html">Próxima <img src="img/icones/seta_direita.png" width="50px"></a>
+					</div>
+				</div>
+			</div>
+		</div>	
+			</div>
+		</div>			
+		<?php include 'template/js-bootstrap.php';?>
+	</body>    
+</html>
--- portfolio.php
+++ portfolio.php
@@ -1,6 +1,3 @@
-<?php 
-	session_start();
-?>
 <!doctype html>
 <html lang="pt-br">
 	<head>
@@ -27,32 +24,12 @@
 	<body class="bg-light">
 		<?php include 'template/menu.php';?>
 		<div class="container conteudo">
-			<div>
-				<div class="d-flex justify-content-center">
-					<h2 class="text-success">Portfólio</h2>
-				</div>
-				<hr class="divisor">
+			<div class="d-flex justify-content-center">
+				<h2 class="text-success">Portfólio</h2>
 			</div>
+			<hr class="divisor">
 			<div>
-				<div class="row">
-					<div class="col-md-6 m-auto">
-						<h5 class="text-center text-primary">Interação Humano-Computador (IHC)</h5>
-					</div>
-				</div>
-				<p class="recuo-primeira-linha">A Interação Humano-Computador estuda a interação entre pessoas e computadores, 
-					assim como as teorias e técnicas de projeto utilizadas para tornar um sistema 
-					interativo. A IHC é baseada tanto no conhecimento da máquina quanto no lado humano.</p>
-				<p class="recuo-primeira-linha">As interações entre usuários e computadores acontecem na interface, portanto, o 
-					design de uma interface tem impacto direto na eficiência dessas interações.</p>
-				<p class="recuo-primeira-linha">O objetivo é minimizar o custo de interação — a quantidade de esforço físico e mental 
-					que um usuário deve exercer ao usar a tecnologia — e tornar as interações mais humanas.
-				</p>
-				<hr class="divisor">
-				<div class="row">
-					<div class="col-md-6 m-auto text-center">
-						<a href="portfolio/portfolio_p-01.html">Próxima <img src="img/icones/seta_direita.png" width="50px"></a>
-					</div>
-				</div>
+				
 			</div>
 		</div>			
 		<?php include 'template/js-bootstrap.php';?>
