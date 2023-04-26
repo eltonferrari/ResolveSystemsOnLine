8d996634861bbe670744242d5c0f66a5bd60be7c
elton ferrari <eltonferrari@gmail.com>
2023-04-26 15:26:08 -0300

    Version-1.2 Finalização de: Login Perfil

--- template/menu.php
+++ template/menu.php
@@ -1,7 +1,7 @@
 <span id='topo'></span>
 <nav class="navbar navbar-expand-md navbar-light bg-light">
     <!-- LOGO -->
-    <a href="\index.php" class="navbar-brand">
+    <a href="https://www.resolvesystems.com.br" class="navbar-brand">
         <img src="\img/logos/logo.png" width="40">
     </a>
     <!-- MENU HAMBURGUER -->
@@ -12,7 +12,17 @@
     <div class="collapse navbar-collapse" id="navbar">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="\index.php">Home</a>
+                <?php
+                    if (isset($_SESSION['logado']) && $_SESSION['logado'] == 1) {
+                ?>
+                        <a class="nav-link" href="\systems/visualizacoes/home/home.php">Home</a>
+                <?php 
+                    } else {
+                ?>
+                        <a class="nav-link" href="\index.php">Home</a>
+                <?php
+                    }
+                 ?>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="\institucional.php">Institucional</a>
@@ -28,6 +38,33 @@
             <li class="nav-item">
                 <a class="nav-link" href="\contato.php">Contato</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="\faleconosco.php">Fale Conosco</a>
+            </li>
+        </ul>
+        <?php
+            if (isset($_SESSION['logado']) && $_SESSION['logado'] == 1) {
+        ?>
+                <ul class="navbar-nav navbar-right">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Acesso</a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="\systems/visualizacoes/pessoas/busca_perfil.php?user=<?= $_SESSION['id_logado'] ?>">Perfil</a>
+                            <a class="dropdown-item" href="\systems/controladores/autenticacao/logoff.php">Sair</a>
+                        </div>
+                    </li>   
+                </ul>
+        <?php
+            } else {
+        ?>
+                <ul class="navbar-nav navbar-right">
+                    <li class="nav-item">
+                        <a class="nav-link" href="\systems/visualizacoes/autenticacao/login.php">Login</a>
+                    </li>
+                </ul>
+        <?php 
+            }
+        ?>
         </ul>
     </div>
 </nav>
